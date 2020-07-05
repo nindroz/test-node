38d9bb6d5ffe12297c093b20c7f9064b191537de
hitesh <mantha.hitesh@gmail.com>
2020-07-04 22:28:13 -0700

    added a post route

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
-exports.get_landing =  function landing (req, res, next) {
+exports.get_landing =  function (req, res, next) {
     res.render('landing', { title: 'Express' });
+  };
+
+  exports.submit_mail =  function (req, res, next) {
+    console.log(req.body.submit_email)
+    res.redirect('/')
   };
